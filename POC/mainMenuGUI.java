2499d2c671c2204752de4a50dc47298e899bab1a
Sean Hobbs <seanphobbs@gmaill.com>
2017-06-21 17:13:20 -0400

    Have started the GUI

---
+++ POC/mainMenuGUI.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class mainMenuGUI {
+	JFrame mainMenu = new JFrame("Main Menu");
+	mainMenu.setVisible(true);
+	mainMenu.setSize(500,300);
+	mainMenu.setDefaultCloseOperation(mainMenu.DISPOSE_ON_CLOSE);
+	
+}
---
+++ POC/src/mainMenuGUI.java
@@ -0,0 +1,7 @@
+import javax.swing.JFrame;
+
+public class mainMenuGUI {
+	JFrame mainMenu = new JFrame("Main Menu");
+	mainMenuGUI().setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+}
